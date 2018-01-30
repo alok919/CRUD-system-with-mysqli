a85e11596485b6222073ec31aa223fd2639c4e39
Alok <arjun@gmail.com>
2018-01-30 23:10:20 +0600

    read data

--- Database.php
+++ Database.php
@@ -10,17 +10,36 @@
 		public $link;
 		public $error;
 
+
+		public function __construct(){
+
+			$this->connectDB();
+		}
+
 		private function connectDB(){
 
 			$this->link = new mysqli($this->dbhost,$this->dbuser,$this->dbpass,$this->dbname);
 
 			if (!$this->link) {
 				$this->error="Connection failed: " . $this->link->connect_error;
-			}else{
-				echo "Connected successfully";
+				return false;
+			}
+
+			}
+
+			public function select($query){
+
+				$result=$this->link->query($query) or die($this->link->error.__LINE__);
+				if ($result->num_rows >0) {
+					
+					return $result;
+				}else{
+
+					return false;
+				}
 			}
 		}
-	}
+	
 
 
 
